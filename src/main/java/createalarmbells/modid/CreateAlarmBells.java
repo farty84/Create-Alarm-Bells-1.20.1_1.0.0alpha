fc6af7ee3efcf167c035ad437243a01b1ab553d2
farty84 <robotboy64x@gmail.com>
2024-11-13 17:56:22 +1100

    n

--- src/main/java/createalarmbells/modid/CreateAlarmBells.java
+++ src/main/java/createalarmbells/modid/CreateAlarmBells.java
@@ -1,5 +1,6 @@
 package createalarmbells.modid;
 
+import createalarmbells.modid.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -18,7 +19,7 @@ public class CreateAlarmBells implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
