8f0f1c423be6a75cccecdee019d6cee1bd3d8fed
farty84 <robotboy64x@gmail.com>
2024-11-13 17:00:52 +1100

    test

---
+++ src/main/java/createalarmbells/modid/CreateAlarmBellsDataGenerator.java
@@ -0,0 +1,11 @@
+package createalarmbells.modid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreateAlarmBellsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
